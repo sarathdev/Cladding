44ca252ded266c1b0559339d1e8da68c57fa71cb
SpurTree <spurtree@SpurTrees-MacBook.local>
2015-04-30 11:22:17 +0530

    Initial Commit

---
+++ Cladding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cladding
+//
+//  Created by SpurTree on 30/04/15.
+//  Copyright (c) 2015 SpurTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Cladding/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cladding
+//
+//  Created by SpurTree on 30/04/15.
+//  Copyright (c) 2015 SpurTree. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
